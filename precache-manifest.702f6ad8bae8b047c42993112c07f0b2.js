6ae530ec5ed8d05d64f8bf9da4b066ac6c69e619
iClouds App Stall <inlaysoft@yahoo.com>
2022-11-01 19:42:56 +0000

    food

---
+++ precache-manifest.702f6ad8bae8b047c42993112c07f0b2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8a78d2a62b6adb71dc2702ebf07c37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d9d899da0615b8a1fd0",
+    "url": "/static/css/2.37daad2d.chunk.css"
+  },
+  {
+    "revision": "6fdbfc886ec9833f63ed",
+    "url": "/static/css/main.bad2de0e.chunk.css"
+  },
+  {
+    "revision": "5d9d899da0615b8a1fd0",
+    "url": "/static/js/2.99e779b8.chunk.js"
+  },
+  {
+    "revision": "279518562c8135f7f7e47b50624fdb9e",
+    "url": "/static/js/2.99e779b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fdbfc886ec9833f63ed",
+    "url": "/static/js/main.9b94aa08.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "667d2b078c7de63f847f44e7edf98c6e",
+    "url": "/static/media/chevronDown.667d2b07.svg"
+  },
+  {
+    "revision": "b0e5851a365f919a6e638f045953c1f9",
+    "url": "/static/media/chevronUp.b0e5851a.svg"
+  },
+  {
+    "revision": "ad1d103b778e53851d09ff5c804b1907",
+    "url": "/static/media/getFetch.ad1d103b.cjs"
+  },
+  {
+    "revision": "2098fcaa8e17280e32c4c5c5472eebf7",
+    "url": "/static/media/img1.2098fcaa.png"
+  },
+  {
+    "revision": "14aa19ca5dfe9cde66da17b1e74f1783",
+    "url": "/static/media/img2.14aa19ca.png"
+  },
+  {
+    "revision": "683fc92cc1f77573b407c41b3a6632f5",
+    "url": "/static/media/img4.683fc92c.png"
+  },
+  {
+    "revision": "eb29cc41c05d8d3bbd49ec3737dd7bce",
+    "url": "/static/media/rolling.eb29cc41.svg"
+  },
+  {
+    "revision": "fc052312bc762c171a58f2c9333ed26c",
+    "url": "/static/media/template1.fc052312.png"
+  },
+  {
+    "revision": "35781737f6536e4df86cfa01d1bd707d",
+    "url": "/static/media/template2.35781737.png"
+  }
+]);
